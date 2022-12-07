a194eeafb48d033a66f1813f40ff38155b8d9416
jaime8793 <109973167+jaime8793@users.noreply.github.com>
2022-12-07 14:50:39 +0300

    Project cards

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -6,7 +6,8 @@ function MyProjects({ myProjects }) {
         <React.Fragment>
             {myProjects.map(project => {
                 return (
-                    <div>
+                   /* <div class= "card">
+                        <du
                         <h2>{project.name}</h2>
                         <h5>{project.description}</h5>
                         {project.comments.map(comment => {
@@ -18,10 +19,30 @@ function MyProjects({ myProjects }) {
                         }
                         )}  
                     </div>
+                )*/
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h2 class="card-title"> {project.name} </h5>
+                        <h5 class= "card text ">{project.description}</h5>
+                        <br></br>
+                        {project.comments.map(comment => {
+                            return (
+                                <div className="card-text">
+                                <p>{comment.opinion}</p>
+                                </div>
+                            )
+                        })}
+
+                        <button type="button" class="btn btn-primary">Edit *remember to enable*</button>
+
+                    </div>
+                </div>
                 )
             })}
         </React.Fragment>
     )
 }
 
+
+                        
 export default MyProjects;
