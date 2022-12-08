b5aaec0343665077e8999c18bfce56df1b9401ed
jaime8793 <109973167+jaime8793@users.noreply.github.com>
2022-12-08 13:30:43 +0300

    Fixed sign up page

--- src/components/User/SignUp.js
+++ src/components/User/SignUp.js
@@ -36,21 +36,17 @@ function SignUp({ setUser, setMember }) {
     return (
 
 
-<section className="vh-100" >
+<section className="vh-90" >
         <div className="container h-100">
             <div className="row d-flex justify-content-center align-items-center h-100">
                 <div className="col-lg-12 col-xl-11">
-                    <div className="card text-black" >
+                    <div className="card text-black" style={{borderRadius: "25px"}}>
                         <div className="card-body p-md-5">
                             <div className="row justify-content-center">
                                 <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
                                         <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
-                                        <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-
-                                                <img src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-registration/draw1.png" className="img-fluid" alt="Sample" />
-
-                                            </div>
+                                        
 
                                         <form onSubmit={handleSubmit} className="mx-1 mx-md-4">
 
@@ -127,6 +123,11 @@ function SignUp({ setUser, setMember }) {
                                             </div>
                                         </form>
                                 </div>
+                                <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                                                <img src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-registration/draw1.png" className="img-fluid" alt="Sample" />
+
+                                            </div>
                             </div>
                         </div>
                     </div>
